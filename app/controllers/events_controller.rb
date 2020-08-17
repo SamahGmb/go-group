6e3f0fda00d246d6c7d87f61ad2632b0b441bb7e
Celine Mercier <mercier_celine@hotmail.fr>
2020-08-17 17:11:17 +0200

    events show page with events title, date(s), location, organizer name and details

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,11 +1,11 @@
 class EventsController < ApplicationController
-
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
   end
 
   def show
+    @event = Event.find(params[:id])
   end
 
 end
