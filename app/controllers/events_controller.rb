943f32c0e7902571f118a63a61ea7937e5d9c9a8
samah giambiasi <samah.elkahlaoui@gmail.com>
2020-08-19 15:27:28 +0200

    update search field

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,13 +8,18 @@ class EventsController < ApplicationController
 
   def show
     @event = Event.find(params[:id])
-    
+
     @marker =
       {
         lat: @event.latitude,
         lng: @event.longitude
-        
+
       }
     @booking = Booking.new
   end
+
+  def search
+    @search = Event.find_by(title: params[:search])
+  end
+
 end
