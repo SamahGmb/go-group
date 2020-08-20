33046da3a613661bd69f51f8cd7e6e7a6105397f
Celine Mercier <mercier_celine@hotmail.fr>
2020-08-20 12:05:32 +0200

    organizer route, controller and views

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,6 @@
 class EventsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
+  before_action :set_event, only: [:show, :edit, :update, :destroy]
 
   def index
     if params[:query].present?
@@ -10,14 +11,49 @@ class EventsController < ApplicationController
     end
   end
 
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.save
+    redirect_to event_path(@event)
+  end
+
   def show
-    @event = Event.find(params[:id])
     @marker =
       {
         lat: @event.latitude,
         lng: @event.longitude
       }
     @booking = Booking.new
+  end
+
+  def my_events
+    @events = events.where(user: current_user)
+  end
+
+  def edit
+  end
+
+  def update
+    @event = Event.update(event_params)
+  end
+
+  def destroy
+    @event.destroy
+    redirect_to my_events_events_path
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :start_date, :end_date, :location, :details, :photo)
+  end
+
+  def set_event
+    @event = Event.find(params[:id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'events#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :events, only: [:show] do
+  resources :events, only: [:new, :create, :show, :edit, :update, :destroy] do
     resources :bookings, only: [:create]
+    collection do
+      get :my_events
+    end
   end
   resources :bookings, only: [:index, :destroy]
 end
